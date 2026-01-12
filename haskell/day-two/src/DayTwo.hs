6d2d12c0bcbb075928c08f061aaac2ce9483c710
Eitan Barylko <eitan@barylko.com>
2026-01-12 12:56:11 -0800

    Added a test

--- haskell/day-two/src/DayTwo.hs
+++ haskell/day-two/src/DayTwo.hs
@@ -1,6 +1,25 @@
 module DayTwo
-    ( someFunc
+    (NonNegative(..), CategorizedId(..), categorizeId
     ) where
+import qualified Control.Applicative as digits
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+
+newtype NonNegative = NonNegative { val:: Int } deriving (Eq, Show)
+
+type UncategorizedId = NonNegative
+
+{-
+This data type represents the two different types of ids in the gift shop
+database, being valid if the id does not consist of a repetition of digits.
+It is invalid otherwise.
+-}
+data CategorizedId = ValidId | InValidId NonNegative deriving (Eq, Show)
+
+{-
+Takes an id that may be valid or invalid, and labels it
+as invalid or valid according to the rules above otherwise.
+-}
+categorizeId :: UncategorizedId -> CategorizedId
+
+categorizeId _ = InValidId $ NonNegative 0
--- haskell/day-two/test/Spec.hs
+++ haskell/day-two/test/Spec.hs
@@ -1,2 +1,10 @@
+import Test.Hspec
+import DayTwo(NonNegative(..), CategorizedId(..), categorizeId)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Categorizing ids" $ do
+    describe "Identifying a valid id" $ do
+      describe "Given a number with an odd number of digits" $ do
+        it "Identifies it as a valid id" $ do
+          categorizeId (NonNegative 3) `shouldBe` ValidId 
