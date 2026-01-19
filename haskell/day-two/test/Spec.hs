328b5a24772b686c33107e888369238cdc8c7583
Eitan Barylko <eitan@barylko.com>
2026-01-19 15:48:40 -0800

    Fixed a test

--- haskell/day-two/test/Spec.hs
+++ haskell/day-two/test/Spec.hs
@@ -18,6 +18,28 @@ mkIdRange :: [Int] -> [UnverifiedProductId]
 
 mkIdRange = map unverifiedId
 
+sampleInput :: [[UnverifiedProductId]]
+
+sampleInput =
+  map
+    mkIdRange
+    [ [11 .. 22],
+      [95 .. 115],
+      [998 .. 1012],
+      [1188511880 .. 1188511890],
+      [222220 .. 222224],
+      [1698522 .. 1698528],
+      [565653 .. 565659],
+      [824824821 .. 824824827],
+      [2121212118 .. 2121212124],
+      [446443 .. 446449],
+      [38593856 .. 38593862]
+    ]
+  
+actualInput :: IO [[UnverifiedProductId]]
+
+actualInput = readFile "data/data.txt" &  fmap  (map parseInput . splitOn ",")
+
 {-
 Takes a string of the form 'a-b', where a and b are both natural numbers, and b > a.
 Returns the range of all unverified ids between a and b.
@@ -56,22 +78,11 @@ main = hspec $ do
           filterInvalidIds (map unverifiedId [565653 .. 565659]) `shouldBe` []
   describe "Summing all invalid ids over a collection of id ranges" $ do
     it "Returns 1227775554 " $ do
-      let ranges = map mkIdRange [[11 .. 22],
-                                  [95 .. 115],
-                                  [998 .. 1012],
-                                  [1188511880 .. 1188511890],
-                                  [222220 .. 222224],
-                                  [1698522 .. 1698528],
-                                  [446443 .. 446449],
-                                  [38593856 .. 38593862]]
-
-      calcInvalidIdsSum ranges `shouldBe` NonNegative 1227775554
+      calcInvalidIdsSum sampleInput `shouldBe` NonNegative 1227775554
 
   describe "Summing all invalid ids over the input id ranges" $ do
     it "Returns 29940924880 " $ do
-      let ranges = readFile "data/data.txt" &  fmap  (map parseInput . splitOn ",")
-
-      calcInvalidIdsSum <$> ranges `shouldReturn` NonNegative 29940924880
+      calcInvalidIdsSum <$> actualInput `shouldReturn` NonNegative 29940924880
 
   describe "Categorizing ids under the new criteria" $ do
     describe "Identifying a valid id" $ do
@@ -88,16 +99,8 @@ main = hspec $ do
 
       describe "Summing all invalid ids over a collection of id ranges" $ do
         it "Returns 4174379265" $ do
-          let ranges = map mkIdRange [[11 .. 22],
-                                      [95 .. 115],
-                                      [998 .. 1012],
-                                      [1188511880 .. 1188511890],
-                                      [222220 .. 222224],
-                                      [1698522 .. 1698528],
-                                      [446443 .. 446449],
-                                      [565653 .. 565659],
-                                      [824824821 .. 824824827],
-                                      [2121212118 .. 2121212124],
-                                      [38593856 .. 38593862]]
+          calcInvalidIdsSum' sampleInput `shouldBe` NonNegative 4174379265
 
-          calcInvalidIdsSum' ranges `shouldBe` NonNegative 4174379265
+      describe "Summing all invalid ids over a large collection of id ranges" $ do
+        it "Returns 48631958998" $ do
+          calcInvalidIdsSum' <$> actualInput `shouldReturn` NonNegative 48631958998
