7858817c44379a59199194ea30f9d356efd27c26
Eitan Barylko <eitan@barylko.com>
2026-02-19 20:36:04 -0800

    Added a test

--- haskell/day-three/test/Spec.hs
+++ haskell/day-three/test/Spec.hs
@@ -10,9 +10,13 @@ mkBattery = Battery . Joltage
 main :: IO ()
 main = hspec $ do
   describe "Finding the most potent battery in a bank" $ do
-    describe "Given a collection of batteries where the third element has the highest voltage" $ do
-      it "The third element and its position is returned" $ do
-        let sample = map mkBattery [1, 2, 3]
+    describe "Given a collection of batteries where the third and fourth battery have the highest voltages" $ do
+      it "The third battery and its position is returned" $ do
+        let sample = map mkBattery [1, 2, 3, 3]
 
         let expected = (2, mkBattery 3)
         findMstPotentBattery sample `shouldBe` expected
+
+
+  describe "Finding the most potent pair of batteries in a bank" $ do
+    it "Returns the combined jolts produced by the batteries "
